360fca7b1146e16b4cbd56b4746b22d21d275f0e
OstappMelnykk <79ostap@ukr.net>
2025-02-28 16:46:42 +0200

    feat(routes): add internal routing for about, auth, home, profile and editor pages

--- CAD-frontend/src/app/features/profile/profile-routing.module.ts
+++ CAD-frontend/src/app/features/profile/profile-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {authGuard} from '../../core/guards/auth.guard';
+import {EditProfileComponent} from './components/edit-profile/edit-profile.component';
+import {ProfileComponent} from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'edit',
+    component: EditProfileComponent,
+    canActivate: [authGuard],
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
