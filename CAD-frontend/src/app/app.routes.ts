eec13a07d4f73e05da58340954fc53515f62ad02
OstappMelnykk <79ostap@ukr.net>
2025-02-28 16:38:50 +0200

    feat(routes): initialize basic app navigation

--- CAD-frontend/src/app/app.routes.ts
+++ CAD-frontend/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import {authGuard} from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule),
+    canActivate: [authGuard], // Доступ лише для авторизованих
+  },
+  {
+    path: '**',
+    redirectTo: 'home', // Резервний маршрут
+  },
+
+];
