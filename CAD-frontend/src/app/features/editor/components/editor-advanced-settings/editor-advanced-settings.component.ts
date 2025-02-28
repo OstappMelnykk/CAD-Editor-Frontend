97c9403e5f59ed401c3b1e43b471811f9629522c
OstappMelnykk <79ostap@ukr.net>
2025-02-28 16:34:14 +0200

    feat: add editor-workspace and editor-advanced-settings components

---
+++ CAD-frontend/src/app/features/editor/components/editor-advanced-settings/editor-advanced-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-advanced-settings',
+  imports: [],
+  templateUrl: './editor-advanced-settings.component.html',
+  styleUrl: './editor-advanced-settings.component.scss'
+})
+export class EditorAdvancedSettingsComponent {
+
+}
---
+++ CAD-frontend/src/app/features/editor/components/editor-workspace/editor-workspace.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-workspace',
+  imports: [],
+  templateUrl: './editor-workspace.component.html',
+  styleUrl: './editor-workspace.component.scss'
+})
+export class EditorWorkspaceComponent {
+
+}
