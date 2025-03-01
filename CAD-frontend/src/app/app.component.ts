3d6c9468794375572433184e886b9d894b4b21e8
OstappMelnykk <79ostap@ukr.net>
2025-03-01 11:03:26 +0200

    feat: add layout for AppComponent with HTML, CSS, and WindowResizeService integration

--- CAD-frontend/src/app/app.component.ts
+++ CAD-frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './shared/components/navbar/navbar.component';
+import {WindowResizeService} from './core/services/ui/window-resize.service';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, NavbarComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'CAD-frontend';
+
+    constructor(private windowResizeService: WindowResizeService) {}
 }
