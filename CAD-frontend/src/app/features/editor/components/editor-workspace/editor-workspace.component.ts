051bc9d1d5454fe4ef38f0df03dae5e35bf50ff6
OstappMelnykk <79ostap@ukr.net>
2025-03-01 11:04:27 +0200

    feat: add layout for EditorWorkspaceComponent with HTML, CSS, and WindowResizeService integration

--- CAD-frontend/src/app/features/editor/components/editor-workspace/editor-workspace.component.ts
+++ CAD-frontend/src/app/features/editor/components/editor-workspace/editor-workspace.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import {WindowResizeService} from '../../../../core/services/ui/window-resize.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-editor-workspace',
-  imports: [],
+    imports: [
+
+    ],
   templateUrl: './editor-workspace.component.html',
   styleUrl: './editor-workspace.component.scss'
 })
 export class EditorWorkspaceComponent {
 
+    isSettingsPanelHidden: boolean = false;
+
+    constructor(private windowResizeService: WindowResizeService) {
+        this.isSettingsPanelHidden = this.windowResizeService.isSettingsPanelHidden;
+    }
+
+    buttons = [
+        { id: 1, title: 'Division', backgroundImg: '.' },
+        { id: 2, title: 'Design', backgroundImg: '.' },
+        { id: 3, title: 'View', backgroundImg: '.' },
+        { id: 4, title: 'Settings', backgroundImg: '.' },
+    ];
 }
