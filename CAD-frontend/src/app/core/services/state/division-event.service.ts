9229354848de68536079a74a7bfd18bfd6c275c1
OstappMelnykk <79ostap@ukr.net>
2025-03-04 17:36:00 +0200

    feat: add event to track "divide" request to the server

---
+++ CAD-frontend/src/app/core/services/state/division-event.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {IAPIData} from '../../interfaces/api/IAPIData.interface';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DivisionEventService {
+
+    private initialAPIData: IAPIData = {
+        points: [],
+        pairsOfIndices:[],
+        polygons:[],
+    }
+
+
+    private divisionEvent = new BehaviorSubject<IAPIData>(this.initialAPIData)
+    private divisionEvent$ = this.divisionEvent.asObservable();
+
+    DivisionOccurs(apiData: IAPIData): void {
+        this.divisionEvent.next(apiData);
+    }
+
+}
