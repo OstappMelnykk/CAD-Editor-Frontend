90634a32390709d30920ddda194e4cb3d704957d
OstappMelnykk <79ostap@ukr.net>
2025-03-28 01:13:59 +0200

    refactor: - organize imports and fields for better clarity - set necessary global variables as class properties for easier access - refactor EventListener with corrected functions - add server request to retrieve default points and initialize a global array for app-wide access - update mesh creation and division logic by creating a reusable function for new mesh creation and integrating it into associated methods

--- CAD-frontend/src/app/features/editor/editor-workspace/components/canvas/canvas.component.ts
+++ CAD-frontend/src/app/features/editor/editor-workspace/components/canvas/canvas.component.ts
@@ -1,21 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {GlobalVariablesService} from '../../../../../core/services/three-js/global-variables.service';
-import {InitService} from '../../../../../core/services/three-js/init.service';
-import {CanvasResizeService} from '../../../../../core/services/three-js/canvas-resize.service';
-import {CameraEventService} from '../../../../../core/services/state/camera-event.service';
-import {ICameraPosition} from '../../../../../core/interfaces/three-js/ICameraPosition.interface';
+import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-import {SuperGeometryMesh} from '../../../../../core/threejsMeshes/SuperGeometryMesh';
-import {forkJoin} from 'rxjs';
-import {IAPIData} from '../../../../../core/interfaces/api/IAPIData.interface';
-import {ApiService} from '../../../../../core/services/api/api.service';
-import {IDivisionRequest} from '../../../../../core/models/DTOs/IDivisionRequest.interface';
-import {DivisionEventService} from '../../../../../core/services/state/division-event.service';
-import {meshOptions} from '../../../../../core/threejsMeshes/meshOptions';
-import {clickToCreate} from './Listeners/clickToCreate';
-import {clickToChoose} from './Listeners/clickToChoose';
-import {meshHover} from './Listeners/meshHover';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { forkJoin } from 'rxjs';
+
+// Services
+import { GlobalVariablesService } from '../../../../../core/services/three-js/global-variables.service';
+import { InitService } from '../../../../../core/services/three-js/init.service';
+import { CanvasResizeService } from '../../../../../core/services/three-js/canvas-resize.service';
+import { CameraEventService } from '../../../../../core/services/state/camera-event.service';
+import { ApiService } from '../../../../../core/services/api/api.service';
+import { DivisionEventService } from '../../../../../core/services/state/division-event.service';
+
+// Interfaces
+import { ICameraPosition } from '../../../../../core/interfaces/three-js/ICameraPosition.interface';
+import { IAPIData } from '../../../../../core/interfaces/api/IAPIData.interface';
+import { IDivisionRequest } from '../../../../../core/models/DTOs/IDivisionRequest.interface';
+
+// Classes & Constants
+import { SuperGeometryMesh } from '../../../../../core/threejsMeshes/SuperGeometryMesh';
+
+// Event Listeners
+import { clickToCreate } from './Listeners/clickToCreate';
+import { mousedownToChoose } from './Listeners/mousedownToChoose';
+import { meshHover } from './Listeners/meshHover';
+import {DEFAULT_POINTS} from '../../../../../core/threejsMeshes/DefaultPoints';
+import {IPoint} from '../../../../../core/interfaces/api/IPoint.interface';
 
 @Component({
     selector: 'app-canvas',
@@ -24,23 +33,34 @@ import {meshHover} from './Listeners/meshHover';
 })
 export class CanvasComponent implements OnInit {
 
-    private objects: THREE.Object3D[] = [];
+    activeObject: SuperGeometryMesh | null = null;
+    hoveredObject: THREE.Object3D | null = null;
+    hoveredPoint: THREE.Vector3 | null = null;
 
-    private raycaster = new THREE.Raycaster();
-    private mouse = new THREE.Vector2();
-    private pickablesObjects: THREE.Object3D[] = [];
+    pickableObjects: THREE.Object3D[] = [];
+    draggableObjects: THREE.Object3D[] = [];
 
-    hoveredObject: SuperGeometryMesh | null = null;
-    activeMesh: SuperGeometryMesh | null = null;
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
 
-    constructor(private globalVariablesService: GlobalVariablesService,
-                private initService: InitService,
-                private canvasResizeService: CanvasResizeService,
-                private cameraEventService: CameraEventService,
-                private apiService: ApiService,
-                private divisionEvent: DivisionEventService) {}
+    canvas!: HTMLCanvasElement;
+    scene!: THREE.Scene;
+    camera!: THREE.PerspectiveCamera;
+    renderer!: THREE.WebGLRenderer;
+    orbitControls!: OrbitControls;
 
-    ngOnInit() {
+
+    constructor(
+        private globalVariablesService: GlobalVariablesService,
+        private initService: InitService,
+        private canvasResizeService: CanvasResizeService,
+        private cameraEventService: CameraEventService,
+        private apiService: ApiService,
+        private divisionEvent: DivisionEventService
+    ) {}
+
+    ngOnInit()
+    {
         const canvas = document.getElementById('canvas') as HTMLCanvasElement
 
         if (!canvas) {
@@ -52,42 +72,102 @@ export class CanvasComponent implements OnInit {
         this.resizeListener()
         this.subscriptionHandler()
 
-        const renderer = this.globalVariablesService.get('renderer') as THREE.WebGLRenderer;
-        const camera = this.globalVariablesService.get('camera') as THREE.PerspectiveCamera;
+        this.canvas = this.globalVariablesService.get('canvas') as HTMLCanvasElement;
+        this.scene = this.globalVariablesService.get('scene') as THREE.Scene;
+        this.camera = this.globalVariablesService.get('camera') as THREE.PerspectiveCamera;
+        this.renderer = this.globalVariablesService.get('renderer') as THREE.WebGLRenderer;
+        this.orbitControls = this.globalVariablesService.get('orbitControls') as OrbitControls;
 
 
-        renderer.domElement.addEventListener('click',(event: MouseEvent) => {
-            clickToCreate(event, {
+        this.renderer.domElement.addEventListener('click',(event: MouseEvent) => {
+            clickToCreate.call(this, event, {
                 setMouse: this.setMouse.bind(this),
-                raycaster: this.raycaster,
-                mouse: this.mouse,
-                camera: camera,
-                pickablesObjects: this.pickablesObjects,
                 createMesh: this.createMesh.bind(this),
             });
         });
 
-        renderer.domElement.addEventListener('click',(event: MouseEvent) => {
-            clickToChoose.call(this, event, {
-                setMouse: this.setMouse.bind(this),
-                raycaster: this.raycaster,
-                mouse: this.mouse,
-                camera: camera,
-                pickablesObjects: this.pickablesObjects,
-            });
+        this.renderer.domElement.addEventListener('mousedown',(event: MouseEvent) => {
+            mousedownToChoose.call(this, event, { setMouse: this.setMouse.bind(this) });
         });
 
-        renderer.domElement.addEventListener('mousemove', (event: MouseEvent) => {
-            meshHover.call(this, event, {
-                setMouse: this.setMouse.bind(this),
-                raycaster: this.raycaster,
-                mouse: this.mouse,
-                camera: camera,
-                pickablesObjects: this.pickablesObjects,
-            });
+        this.renderer.domElement.addEventListener('mousemove', (event: MouseEvent) => {
+            this.hoveredPoint = meshHover.call(this, event, { setMouse: this.setMouse.bind(this) });
+        });
+
+        this.apiService.DefaultPoints().subscribe({
+            next: (defaultPoints) => {
+                DEFAULT_POINTS.push(...defaultPoints as IPoint[])
+                DEFAULT_POINTS.sort((a, b) => a.localIds[0] - b.localIds[0]);
+
+                console.log(DEFAULT_POINTS)
+            },
+            error: (err) => console.error('Error while fetching DefaultPoints:', err),
         });
     }
 
+
+    async onDivisionOcures(apiData: IAPIData)
+    {
+        if (this.activeObject === null) {
+            console.log("No active mesh");
+            return;
+        }
+
+        const activeMeshPosition: THREE.Vector3 = this.activeObject.position;
+        const activeMeshDefaultPoints = (this.activeObject as SuperGeometryMesh).defaultPoints
+
+        if (this.activeObject && this.activeObject.parent) {
+            this.scene.remove(this.activeObject);
+            this.activeObject.dispose();
+        }
+        this.activeObject = null;
+
+        const index1 = this.pickableObjects.findIndex(obj => obj.position.equals(activeMeshPosition));
+        if (index1 > -1) this.pickableObjects.splice(index1, 1);
+
+
+        const superGeometryMesh = this.createAndAddMesh(apiData, activeMeshPosition);
+    }
+
+
+    private createAndAddMesh(apiData: IAPIData, position: THREE.Vector3): SuperGeometryMesh
+    {
+        const superGeometryMesh = new SuperGeometryMesh(this.apiService, this.globalVariablesService);
+        superGeometryMesh.createMesh(apiData);
+
+        superGeometryMesh.position.copy(position);
+
+        this.pickableObjects.push(superGeometryMesh);
+        this.draggableObjects.push(superGeometryMesh);
+
+        this.scene.add(superGeometryMesh);
+        return superGeometryMesh
+    }
+
+
+    createMesh(): void
+    {
+        const divisionRequest = { x: 1, y: 1, z: 1 } as IDivisionRequest
+
+        this.apiService.Divide(divisionRequest).subscribe({
+            next: () => {
+                forkJoin({
+                    points: this.apiService.Points(),
+                    pairsOfIndices: this.apiService.PairsOfIndices(),
+                    polygons: this.apiService.Polygons(),
+                    defaultComplexPoints: this.apiService.DefaultPoints(),
+                }).subscribe({
+                    next: (apiData: IAPIData) => {
+                        const position = new THREE.Vector3();
+                        this.raycaster.ray.at(10, position);
+                        this.createAndAddMesh(apiData, position)
+                    },
+                    error: (err) => console.error('Error while fetching mesh data:', err),
+                });
+            },
+            error: (err) => {console.error('Error while execution Divide:', err);}
+        });
+    }
 
 
     subscriptionHandler(){
@@ -96,7 +176,7 @@ export class CanvasComponent implements OnInit {
         })
 
         this.divisionEvent.divisionEvent$.subscribe((apiData) => {
-                this.onDivisionOcures(apiData);
+            this.onDivisionOcures(apiData);
         })
     }
 
@@ -105,10 +185,8 @@ export class CanvasComponent implements OnInit {
         window.dispatchEvent(new Event('resize'));
     }
 
-
     setMouse(event: MouseEvent): void{
-        const renderer = this.globalVariablesService.get('renderer') as THREE.WebGLRenderer;
-        const rect = renderer.domElement.getBoundingClientRect();
+        const rect = this.renderer.domElement.getBoundingClientRect();
         const mouseX = ((event.clientX - rect.left) / this.canvasResizeService.canvasSize.Width!) * 2 - 1;
         const mouseY = -((event.clientY - rect.top) / this.canvasResizeService.canvasSize.Height!) * 2 + 1;
         this.mouse.set(mouseX, mouseY);
@@ -116,79 +194,9 @@ export class CanvasComponent implements OnInit {
 
     onCameraPositionChanged(cameraPosition: ICameraPosition): void
     {
-        const _camera = this.globalVariablesService.get('camera') as THREE.PerspectiveCamera | null;
-        const _orbitControls = this.globalVariablesService.get('orbitControls') as OrbitControls | null;
-
-        _camera?.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
-        _camera?.lookAt(0, 0, 0);
-        _orbitControls?.target.set(0, 0, 0);
-        _orbitControls?.update();
-    }
-
-
-    onDivisionOcures(apiData: IAPIData)
-    {
-        if(this.activeMesh === null){
-            console.log("No active mesh")
-            return;
-        }
-
-
-        let scene = this.globalVariablesService.get('scene') as THREE.Scene;
-        const activeMeshPosition: THREE.Vector3 = this.activeMesh.position
-        scene.remove(this.activeMesh)
-        this.activeMesh.dispose()
-        this.activeMesh = null
-
-        const index1 = this.pickablesObjects.findIndex(obj => obj.position.equals(activeMeshPosition));
-        if (index1 > -1) {
-            this.pickablesObjects.splice(index1, 1);
-        }
-
-        const superGeometryMesh = new SuperGeometryMesh(this.apiService);
-        superGeometryMesh.createMesh(apiData, meshOptions)
-
-        superGeometryMesh.position.copy(activeMeshPosition);
-
-        this.objects.push(superGeometryMesh);
-        this.pickablesObjects.push(superGeometryMesh);
-
-        scene.add(superGeometryMesh);
-    }
-
-    createMesh(): void
-    {
-        const divisionRequest = {
-            x: 1,
-            y: 1,
-            z: 1,
-        } as IDivisionRequest
-
-        this.apiService.Divide(divisionRequest).subscribe({
-            next: () => {
-                forkJoin({
-                    points: this.apiService.Points(),
-                    pairsOfIndices: this.apiService.PairsOfIndices(),
-                    polygons: this.apiService.Polygons()
-                }).subscribe((apiData: IAPIData) => {
-
-                    const superGeometryMesh = new SuperGeometryMesh(this.apiService);
-                    superGeometryMesh.createMesh(apiData, meshOptions)
-
-                    const position = new THREE.Vector3();
-                    this.raycaster.ray.at(10, position);
-                    superGeometryMesh.position.copy(position);
-
-                    this.objects.push(superGeometryMesh);
-                    this.pickablesObjects.push(superGeometryMesh);
-
-                    let scene = this.globalVariablesService.get('scene') as THREE.Scene;
-                    scene.add(superGeometryMesh);
-                });
-            },
-            error: (err) => {
-                console.error('Error while execution Divide:', err);
-            }
-        });
+        this.camera?.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+        this.camera?.lookAt(0, 0, 0);
+        this.orbitControls?.target.set(0, 0, 0);
+        this.orbitControls?.update();
     }
 }
