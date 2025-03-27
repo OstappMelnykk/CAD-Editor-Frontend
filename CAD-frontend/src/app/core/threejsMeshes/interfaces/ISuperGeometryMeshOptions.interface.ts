6cad1551d1d463eafad5707e4fb459eb61999331
OstappMelnykk <79ostap@ukr.net>
2025-03-28 01:04:25 +0200

    fix: correct fields in SuperGeometryMeshOptions interface

--- CAD-frontend/src/app/core/threejsMeshes/interfaces/ISuperGeometryMeshOptions.interface.ts
+++ CAD-frontend/src/app/core/threejsMeshes/interfaces/ISuperGeometryMeshOptions.interface.ts
@@ -3,28 +3,9 @@ import {IMeshColors} from './IMeshColors.interface';
 
 export interface SuperGeometryMeshOptions {
     colors: IMeshColors;
-    opacity: number;
+    mehsOpacity: number;
+    wireframeOpacity: number;
     wireframe: boolean;
     depthWrite: boolean;
     depthTest: boolean;
 }
-
-
-/*
-?? {
-            colors: {
-                defaultColor: new THREE.Color('#5a8be2'),
-                hoverColor: new THREE.Color('rgba(255,4,4,0.7)'),
-                activeColor: new THREE.Color('rgba(0,89,255,0.61)'),
-                linesegmentsDefaultColor: new THREE.Color(0xbfc2c7),
-                sphereDefaultColor: new THREE.Color(0xbfc2c7),
-                sphereDraggableColor: new THREE.Color(0xbfc2c7)
-            } as IMeshColors,
-
-            opacity: 0.2,
-            wireframe: false,
-            depthWrite: false,
-            depthTest: true,
-        }
-
-*/
