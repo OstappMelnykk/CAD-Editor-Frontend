aaf393a8c99f1c811c16bb67718a6471cb4c6d4a
OstappMelnykk <79ostap@ukr.net>
2025-04-22 00:40:43 +0300

    feat: add ColorSettings enum and integrate color changing events
    - Created ColorSettings enum for managing component color settings
    - Followed best practices for integrating the enum
    - Incorporated color-changing events into the component

--- CAD-frontend/src/app/features/editor/editor-workspace/components/settings-panel/design/design.component.ts
+++ CAD-frontend/src/app/features/editor/editor-workspace/components/settings-panel/design/design.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {ColorPickerDirective} from 'ngx-color-picker';
+import {DivisionEventService} from '../../../../../../core/services/state/division-event.service';
+import {ColorsEventService} from '../../../../../../core/services/state/colors-event.service';
+import {IDivisionConfig} from '../../../../../../core/interfaces/api/IDivisionConfig';
 
 @Component({
     selector: 'app-design',
@@ -15,59 +18,112 @@ import {ColorPickerDirective} from 'ngx-color-picker';
 })
 export class DesignComponent {
     title = 'Color Picker Settings';
-
-    // Динамічні змінні для кольорів
-    defaultMeshColor: string = '#fff';
-    hoverMeshColor: string = '#fff';
-    activeMeshColor: string = '#fff';
-    wireframeColor: string = '#fff';
-    defaultSphereColor: string = '#fff';
-    draggableSphereColor: string = '#fff';
-
-    meshOpacity: number = 0.5;
-
-
+    settingsEnum = ColorSettings;
     settingsForm: FormGroup;
 
-    constructor(private fb: FormBuilder) {
-        // Ініціалізуємо значення кожного поля в формі
+    constructor(private fb: FormBuilder, private colorsEvent: ColorsEventService,) {
         this.settingsForm = this.fb.group({
-            defaultMeshColor: [this.defaultMeshColor],
-            hoverMeshColor: [this.hoverMeshColor],
-            activeMeshColor: [this.activeMeshColor],
-            wireframeColor: [this.wireframeColor],
-            defaultSphereColor: [this.defaultSphereColor],
-            draggableSphereColor: [this.draggableSphereColor],
-            meshOpacity: [this.meshOpacity] // Додаємо контрол для mesh opacity
+            [ColorSettings.DefaultMeshColor]: ['#fff'],
+            [ColorSettings.HoverMeshColor]: ['#fff'],
+            [ColorSettings.ActiveMeshColor]: ['#fff'],
+            [ColorSettings.WireframeColor]: ['#fff'],
+            [ColorSettings.DefaultSphereColor]: ['#fff'],
+            [ColorSettings.DraggableSphereColor]: ['#fff'],
+            [ColorSettings.MeshOpacity]: [0.1]
         });
     }
 
-    ngOnInit(): void {
-        // Відслідковування змін для кожного поля
+    /*ngOnInit(): void {
         Object.keys(this.settingsForm.controls).forEach((controlName) => {
             this.settingsForm.get(controlName)?.valueChanges.subscribe((newValue: any) => {
                 console.log(`${controlName}: Нове значення - ${newValue}`);
-                (this as any)[controlName] = newValue; // Оновлюємо локальну змінну
+                (this as any)[controlName] = newValue;
+            });
+        });
+    }*/
+
+    ngOnInit(): void {
+        // Слухач змін для кожного поля
+        Object.keys(this.settingsForm.controls).forEach((controlName) => {
+            this.settingsForm.get(controlName)?.valueChanges.subscribe((newValue) => {
+                console.log(`${controlName}: Нове значення - ${newValue}`);
+                this.handleSettingChange(controlName as ColorSettings, newValue);
             });
         });
     }
 
-    onColorChange(field: string, newColor: string): void {
-        console.log(`colorPickerChange: ${field} - Новий колір: ${newColor}`);
-        this.settingsForm.get(field)?.setValue(newColor, { emitEvent: false }); // Оновлюємо форму
+
+    handleSettingChange(setting: ColorSettings, value: string | number): void {
+        switch (setting) {
+            case ColorSettings.DefaultMeshColor:
+                this.colorsEvent.ChangeDefaultMeshColor(value as string);
+                break;
+            case ColorSettings.HoverMeshColor:
+                this.colorsEvent.ChangeHoverMeshColor(value as string);
+                break;
+            case ColorSettings.ActiveMeshColor:
+                this.colorsEvent.ChangeActiveMeshColor(value as string);
+                break;
+            case ColorSettings.WireframeColor:
+                this.colorsEvent.ChangeWireframeColor(value as string);
+                break;
+            case ColorSettings.DefaultSphereColor:
+                this.colorsEvent.ChangeDefaultSphereColor(value as string);
+                break;
+            case ColorSettings.DraggableSphereColor:
+                this.colorsEvent.ChangeDraggableSphereColor(value as string);
+                break;
+            case ColorSettings.MeshOpacity:
+                this.colorsEvent.ChangeMeshOpacity(+value); // Перетворення в число
+                break;
+        }
     }
 
-    // Відслідковування змін прозорості
+    onColorChange(setting: ColorSettings, newColor: string): void {
+        console.log(`colorPickerChange: ${setting} - Новий колір: ${newColor}`);
+        this.settingsForm.get(setting)?.setValue(newColor, { emitEvent: false });
+    }
+
     onOpacityChange(event: Event): void {
-        const inputElement = event.target as HTMLInputElement; // Приведення до HTMLInputElement
-        const newOpacity = parseFloat(inputElement.value); // Отримуємо значення
+        const inputElement = event.target as HTMLInputElement;
+        const newOpacity = parseFloat(inputElement.value);
         console.log(`meshOpacity: Нове значення - ${newOpacity}`);
-        this.meshOpacity = newOpacity; // Оновлюємо локальну змінну
-        this.settingsForm.get('meshOpacity')?.setValue(newOpacity); // Оновлюємо форму
+        this.settingsForm.get(ColorSettings.MeshOpacity)?.setValue(newOpacity);
     }
 
 
 
+  /*  onColorChange(field: string, newColor: string): void {
+        console.log(`colorPickerChange: ${field} - Новий колір: ${newColor}`);
+       /!* this.colorsEvent.
+        this.divisionEvent.DivisionOccurs(divisionRequest as IDivisionConfig);*!/
+        this.settingsForm.get(field)?.setValue(newColor, { emitEvent: false }); // Оновлюємо форму
+    }*/
 
 
+    /*ChangeDefaultMeshColor(defaultMeshColor: string)
+    ChangeHoverMeshColor(hoverMeshColor: string)
+    ChangeActiveMeshColor(activeMeshColor: string): void {th
+    ChangeWireframeColor(wireframeColor: string): void {this
+    ChangeDefaultSphereColor(defaultSphereColor: string): vo
+    ChangeDraggableSphereColor(draggableSphereColor: string)
+    ChangeMeshOpacity(meshOpacity: number): void {this.meshO*/
+
+   /* onOpacityChange(event: Event): void {
+        const inputElement = event.target as HTMLInputElement;
+        const newOpacity = parseFloat(inputElement.value);
+        console.log(`meshOpacity: Нове значення - ${newOpacity}`);
+        this.meshOpacity = newOpacity;
+        this.settingsForm.get('meshOpacity')?.setValue(newOpacity);
+    }*/
+}
+
+export enum ColorSettings {
+    DefaultMeshColor = 'defaultMeshColor',
+    HoverMeshColor = 'hoverMeshColor',
+    ActiveMeshColor = 'activeMeshColor',
+    WireframeColor = 'wireframeColor',
+    DefaultSphereColor = 'defaultSphereColor',
+    DraggableSphereColor = 'draggableSphereColor',
+    MeshOpacity = 'meshOpacity'
 }
